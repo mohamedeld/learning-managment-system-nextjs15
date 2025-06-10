23cfb0d35eacb0f46f70b83aeb3d9961f0c47632
mohamedeld <mohamed.azoz20010@gmail.com>
2025-06-10 20:25:49 +0300

    test

--- drizzle/schema/courseSesction.ts
+++ drizzle/schema/courseSesction.ts
@@ -5,7 +5,6 @@ import { CourseTable } from "./course";
 export const courseSectionStatus = ["public","private"] as const;
 export type CourseSestionStatus = (typeof courseSectionStatus)[number];
 export const courseSectionsEnum = pgEnum("course_section_status",courseSectionStatus)
-// course table
 export const CourseSectionTable = pgTable("courseSections",{
     id,
     name:text().notNull(),
